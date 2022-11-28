9166dbb1408c698667973f165c135018398d3ec8
MarkMusyoka <102751519+MarkMusyoka@users.noreply.github.com>
2022-11-28 18:27:25 +0300

    Update MZURI PAYROLL SYSTEM.java

--- MZURI PAYROLL SYSTEM.java
+++ MZURI PAYROLL SYSTEM.java
@@ -16,4 +16,44 @@ passLabel=new JLabel()
 passLabel.setText("Password");
 texField2=new JPasswordField(15);
 b1=new JButton("SUBMIT")
+newPanel=new Jpane(new GridLayout(3,1))
+newPanel.add(userLabel);
+newPanel.add(textfield1);
+newPanel.add(passLabel);
+newPanel.add(textField2);
+newPanel.add(b1);
+add(newPanel,BorderLayout.CENTER);
+b1.addActionListener(this);
+setTitle("LOGIN TO MZURI PAYROLL");
+}
+public void actionPerformed(ActionEvent ae)
+{
+  String userValue=textField1.getText();
+  String passValue=textField2.getText();
+if(userValue.equals("test1@gmal.com")&&passValue.equals("test")){
+NewPage page=new NewPage();
+  page.setVisible(true);
+ JLabel wel_label=new JLabel("Welcome:"+userValue);
+ page.getContentPane().add(wel_label);
+}
+  else{
+    system.out.printlm("Please enter valid username and password")
+    }
+}
+}
+class LoginformDemo
+{
+  public static void main (String arg[])
+  {
+    try
+    {
+     createLoginForm form=new CreateLoginForm();
+      form.setSizeVisible(true);
+    }
+    catch(Exception e)
+    {
+   JoptionPane.showMessageDialog(null,e.getMessage());
+    }
+  }
+}
 
